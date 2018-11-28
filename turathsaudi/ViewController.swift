8bee89254a644ce468f11fdf38f8c8d1a24e811b
عماد صلاح <alemad2000@gmail.com>
2018-11-28 11:55:25 +0300

    Initial Commit

---
+++ turathsaudi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  turathsaudi
+//
+//  Created by Emad salah on 28/11/2018.
+//  Copyright © 2018 Emad salah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
